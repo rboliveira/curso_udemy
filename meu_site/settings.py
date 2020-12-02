85f59c176eff8a3c840a679771681380cfeae71f
rboliveira <rboliveira.rs@gmail.com>
2020-12-02 15:09:54 -0300

    Update settings.py

--- meu_site/settings.py
+++ meu_site/settings.py
@@ -114,8 +114,7 @@ USE_L10N = True
 USE_TZ = True
 
 
-# Static files (CSS, JavaScript, Images)
+# Static files (CSS, JavaScript, Images) TESTE GITHUB
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
 STATIC_URL = '/static/'
-# TESTE GITHUB
